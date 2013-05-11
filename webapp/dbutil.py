5add5ba2562de4ec9e3e0dcb706780b8bc3d414a
aymanfarhat <ayman.farhat1@gmail.com>
2013-05-11 22:19:59 +0300

    proper JSON response

--- webapp/dbutil.py
+++ webapp/dbutil.py
@@ -13,7 +13,6 @@ database = "redditmon"
 # Fields to include and exclude in any query on the logs
 logs_projection = {"subscribers":1,"readers":1,"time":1,"_id":0}
 
-
 def datetime_handler(obj):
 	""" Properly formats a datetime field for json_dumps """
 	if isinstance(obj,datetime):
@@ -30,11 +29,11 @@ def get_log_single(subreddit,date):
 	client = MongoClient()
 	db = client[database]
 	results = db[subreddit].find({'time':{"$gte":date,"$lt":date+timedelta(days=1)}},logs_projection)
-	return [json.dumps(doc, default = datetime_handler) for doc in results]
+	return json.dumps([json.dumps(doc, default = datetime_handler) for doc in results])
 
 def get_log_range(subreddit,start_date,end_date):
 	""" Gets subreddit logs of all days within the selected range inclusive """
 	client = MongoClient()
 	db = client[database]
 	results = db[subreddit].find({'time':{"$gte":start_date,"$lt":end_date+timedelta(days=1)}},logs_projection)
-	return [json.dumps(doc, default = datetime_handler) for doc in results]
+	return json.dumps([json.dumps(doc, default = datetime_handler) for doc in results])
--- webapp/redditmon.py
+++ webapp/redditmon.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 import dbutil
 from datetime import datetime
+import json 
 
 # Configuration
 DEBUG = True
@@ -16,18 +17,22 @@ def index():
 
 @app.route("/logs")
 def get_logs():
+	# parameter values
 	subreddit = request.args.get('subreddit',None)
 	start = request.args.get('start', None)
 	end = request.args.get('end', None)
+	
+	# Default reply
+	reply = '{"status":"fail","data":"Invalid parameters"}'
 
 	if subreddit != None and start != None and end != None:
 		if start == end:
 			resultset = dbutil.get_log_single(subreddit,datetime.strptime(start, '%Y-%m-%d'))
 		else:
 			resultset = dbutil.get_log_range(subreddit,datetime.strptime(start,'%Y-%m-%d'),datetime.strptime(end,'%Y-%m-%d'))
-		return str(resultset)
-	else:
-		return "Invalid parameters"
+		reply = '{"status":"success","data":{"logs":'+resultset+'}}'
+	
+	return Response(reply,status=200,mimetype='application/json')
 
 if __name__ == "__main__":
 	app.run()
