73aa352dbc0f6e0dbcea9d4964e8ea2c0d96befb
aymanfarhat <ayman.farhat1@gmail.com>
2013-05-11 20:05:32 +0300

    Added logs view and logs querying functions

--- webapp/dbutil.py
+++ webapp/dbutil.py
@@ -1,14 +1,31 @@
-"""Simple module for abstracting the mongo queries in to callable functions"""
+"""Simple module for abstracting the mongo queries in to callable 
+functions and serializing results to JSON format"""
 import pymongo
 from pymongo import MongoClient
+from datetime import datetime
+from datetime import timedelta
+from bson import json_util
+import json
 
 # Some settings
-db = "redditmon"
+database = "redditmon"
 
 def get_subreddits():
 	""" Gets a list of all available subreddits in the db """
 	client = MongoClient()
-	db = client["redditmon"]
+	db = client[database]
 	return filter(lambda x: x != "system.indexes", map(str,db.collection_names()))
 	
+def get_log_single(subreddit,date):
+	""" Gets all logs of a subreddit on a single date """
+	client = MongoClient()
+	db = client[database]
+	results = db[subreddit].find({'time':{"$gte":date,"$lt":date+timedelta(days=1)}})
+	return [json.dumps(doc, default=json_util.default) for doc in results]
 
+def get_log_range(subreddit,start_date,end_date):
+	""" Gets subreddit logs of all days within the selected range inclusive """
+	client = MongoClient()
+	db = client[database]
+	results = db[subreddit].find({'time':{"$gte":start_date,"$lt":end_date+timedelta(days=1)}})
+	return [json.dumps(doc, default=json_util.default) for doc in results]
--- webapp/redditmon.py
+++ webapp/redditmon.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import dbutil
+from datetime import datetime
 
 # Configuration
 DEBUG = True
@@ -12,6 +13,21 @@ app.config.from_envvar('FLASK_SETTINGS', silent=True)
 def index():
 	subreddits = dbutil.get_subreddits()
 	return render_template("index.html",subreddits=subreddits)
+
+@app.route("/logs")
+def get_logs():
+	subreddit = request.args.get('subreddit',None)
+	start = request.args.get('start', None)
+	end = request.args.get('end', None)
+
+	if subreddit != None and start != None and end != None:
+		if start == end:
+			resultset = dbutil.get_log_single(subreddit,datetime.strptime(start, '%Y-%m-%d'))
+		else:
+			resultset = dbutil.get_log_range(subreddit,datetime.strptime(start,'%Y-%m-%d'),datetime.strptime(end,'%Y-%m-%d'))
+		return str(resultset)
+	else:
+		return "Invalid parameters"
 
 if __name__ == "__main__":
 	app.run()
