8106e317dd29b338e3c98b4cd8559afe0c0c4858
aymanfarhat <ayman.farhat1@gmail.com>
2013-05-11 20:36:36 +0300

    Added projection for log queriesw

--- webapp/dbutil.py
+++ webapp/dbutil.py
@@ -10,6 +10,9 @@ import json
 # Some settings
 database = "redditmon"
 
+# Fields to include and exclude in any query on the logs
+logs_projection = {"subscribers":1,"readers":1,"time":1,"_id":0}
+
 def get_subreddits():
 	""" Gets a list of all available subreddits in the db """
 	client = MongoClient()
@@ -20,12 +23,12 @@ def get_log_single(subreddit,date):
 	""" Gets all logs of a subreddit on a single date """
 	client = MongoClient()
 	db = client[database]
-	results = db[subreddit].find({'time':{"$gte":date,"$lt":date+timedelta(days=1)}})
+	results = db[subreddit].find({'time':{"$gte":date,"$lt":date+timedelta(days=1)}},logs_projection)
 	return [json.dumps(doc, default=json_util.default) for doc in results]
 
 def get_log_range(subreddit,start_date,end_date):
 	""" Gets subreddit logs of all days within the selected range inclusive """
 	client = MongoClient()
 	db = client[database]
-	results = db[subreddit].find({'time':{"$gte":start_date,"$lt":end_date+timedelta(days=1)}})
+	results = db[subreddit].find({'time':{"$gte":start_date,"$lt":end_date+timedelta(days=1)}},logs_projection)
 	return [json.dumps(doc, default=json_util.default) for doc in results]
