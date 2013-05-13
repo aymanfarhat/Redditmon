e989b1531f0da0b960246fcce74ed662550e52ac
aymanfarhat <ayman.farhat1@gmail.com>
2013-05-13 19:25:57 +0300

    Updated date validation regex check before int val check to avoid exception in string to int conversion

--- webapp/validate.py
+++ webapp/validate.py
@@ -2,8 +2,8 @@
 import re 
 
 def date(dt):
-	dt_lst = map(int,dt.split("-"))
-	print dt_lst
-	if (dt_lst[1] >= 1 and dt_lst[1] <= 12) and (dt_lst[2] >= 1 and dt_lst[2] <= 31):
-		return bool(re.search(r"^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$",dt))	
-	return False
+	if bool(re.search(r"^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$",dt)):		
+		dt_lst = map(int,dt.split("-"))
+		if (dt_lst[1] >= 1 and dt_lst[1] <= 12) and (dt_lst[2] >= 1 and dt_lst[2] <= 31):
+			return True
+		return False
