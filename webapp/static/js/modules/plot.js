2c7edf2bd0d3e9ac13c196dfadb542907abb4c8c
aymanfarhat <ayman.farhat1@gmail.com>
2013-05-22 13:55:43 +0300

    Added plot point hover

--- webapp/static/js/modules/plot.js
+++ webapp/static/js/modules/plot.js
@@ -1,36 +1,87 @@
 /* Module for dealing with the plot */
 var PlotModule = (function(window,$)
 {
-	var init = function(){};
+	var s = {
+		placeHolder:$("#placeholder"),
+	};
+
+	var init = function()
+	{
+		BindUIActions();
+	};
+
+	var BindUIActions = function()
+	{
+		s.placeHolder.bind("plothover", function (event, pos, item) 
+		{
+			if(item) 
+			{
+				if (previousPoint != item.dataIndex) 
+				{
+					previousPoint = item.dataIndex;
+					
+					$("#tooltip").remove();
+
+					var x = item.datapoint[0],
+						y = item.datapoint[1];
+					
+					showTooltip(item.pageX, item.pageY,parseInt(y));
+				}
+			}
+			else {
+				$("#tooltip").remove();
+				previousPoint = null;            
+			}
+		});
+		
+		s.placeHolder.bind("plotclick",function(event, pos, item){
+			console.log("click event");	
+		});
+	};
 
 	var plot = function(logs)
 	{
 		var readers = [];
 		
-		_.each(logs,function(log,i){
+		_.each(logs,function(log, i){
 			readers.push([moment.utc(log.time,"YYYY-MM-DD HH:mm"),log.readers]);
 		});
 		
-		var settings = [
-		{
+		var settings = [{
 			data: readers,
 			color: '#FFAA42',
 			label:'Readers',
 			lines:{show:true},
-			points:{show:true}
-		},];
+			points:{show:true},
+			}];
 
 		var options = {
-			xaxis: { mode: "time"}
+			xaxis: {mode:"time"},
+			grid:{hoverable:true,clickable:true}
 		};
 
-		$.plot("#placeholder",settings,options);
+		$.plot(s.placeHolder,settings,options);
 
 		/*
 		Need to check later wether reader or subscriber selected
 		$("#categswitch > button.active").text());
 		*/
 	};
+
+ 	var showTooltip = function(x, y, contents) 
+	{
+       $('<div id="tooltip">'+contents+'</div>').css({
+            'position': 'absolute',
+            'display': 'none',
+            'top': y-30,
+            'left': x-30,
+            'border': '1px solid #CCC',
+            'padding': '2px',
+            'background-color': '#FFF',
+            'opacity': 0.80,
+			'font-size':'20px'
+        }).appendTo("body").fadeIn(200);
+    }
 
 	return {
 		init: init,
