c809210bfa8341077401b6cf4a5e52b7ecc9e5dd
aymanfarhat <ayman.farhat1@gmail.com>
2013-05-25 17:50:43 +0300

    reply as serialized dict instead of strings

--- webapp/redditmon.py
+++ webapp/redditmon.py
@@ -24,16 +24,32 @@ def get_logs():
     end = request.args.get('end', None)
     
     # Default reply
-    reply = '{"status":"fail","data":"The parameters or values you submitted are invalid, please recheck your input."}'
+    reply = {
+        "status":"fail",
+        "data":"The parameters/values submitted are invalid, recheck your input."
+    }
 
     if validate.nonempty_list_vals([subreddit,start,end]) and validate.date(start) and validate.date(end):
+ 
+        start_datetime = datetime.strptime(start,'%Y-%m-%d')
+        end_datetime = datetime.strptime(end,'%Y-%m-%d')
+
         if start == end:
-            resultset = dbutil.get_log_single(subreddit,datetime.strptime(start, '%Y-%m-%d'))
+            resultset = dbutil.get_log_single(subreddit,start_datetime)
         else:
-            resultset = dbutil.get_log_range(subreddit,datetime.strptime(start,'%Y-%m-%d'),datetime.strptime(end,'%Y-%m-%d'))
-        reply = '{"status":"success","data":{"start":"'+start+'","end":"'+end+'","logs":'+resultset+'}}'
-    
-    return Response(reply,status=200,mimetype='application/json')
+            resultset = dbutil.get_log_range(subreddit,start_datetime,end_datetime)
+
+        # Success reply
+        reply = {
+            "status":"success",
+            "data":{
+                "start":start,
+                "end":end,
+                "logs":resultset
+            }
+        }
+
+    return Response(json.dumps(reply),status=200,mimetype='application/json')
 
 if __name__ == "__main__":
     app.run()
